652c58cfcea7fede6520ef72005e6c1d11587c54
Kevin Topollaj <kevintopollaj@gmail.com>
2021-04-18 19:55:02 +0200

    new GenericNetworkManager Xcode project

---
+++ GenericNetworkManager/GenericNetworkManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericNetworkManager
+//
+//  Created by Kevin Topollaj on 18.4.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
